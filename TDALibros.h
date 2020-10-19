f9c4425c7723336497cecea2e2fac21d453d5063
Albert-PG <cecyt.cecyt@gmail.com>
2020-10-18 20:21:47 -0500

    Create the firsts elements

--- TDALibros.h
+++ TDALibros.h
@@ -8,3 +8,32 @@ struct hoja{
 struct Libro{
   struct hoja objHoja;
 };
+
+//Catalogo
+//struct Libro catalogo [33];
+
+struct Libro objLibro[]={{8.5,1.18,"El avestrus", "Arial"},
+                         {8.5,0.74,"La niña", "Tahoma"},
+                         {6.5,1.78,"El cuento", "Calibri"},
+                         {5.5, 1.86,"Anécdota de un día", "Tahoma Black"},
+                         {4.5,0.58,"Cien años y uno mas","Abscissa"}/*,
+                         {,,,},
+                         {,,,},
+                         {,,,},
+                         {,,,},
+                         {,,,},
+                         {},
+                         {},
+                         {},
+
+                         {},
+                         {},
+                         {},
+                         {},
+                         {},
+                         {},
+                         {},
+                         {},
+                         {},
+                         {},*/
+                       };
