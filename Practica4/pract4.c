3f16a3e2a8e7664f1e34e6c740d1332779ad372f
Albert-PG <cecyt.cecyt@gmail.com>
2020-11-05 16:53:27 -0600

    Update generator and set polynomial generator test

--- Practica4/pract4.c
+++ Practica4/pract4.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
 #define semilla 7
 
+//Global variable
+int i;
 //Funtion Declarations
 int generator();
 
+
+//Polynomial Generator Test
+//x^4 + x^3 + 7x +1
 int main(){
 
-
   return 0;
+}
+
+int generator(){
+  int potGeneradora = 5;
+  int xo= pow(semilla, potGeneradora);
+
 }
